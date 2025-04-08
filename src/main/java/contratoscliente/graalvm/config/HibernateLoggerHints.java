d9e7573003600371bd3ffeb03dbee1f564213132
gamartins01311 <gamartins013@gmail.com>
2025-04-08 19:45:06 -0300

    correcao hints

--- src/main/java/contratoscliente/graalvm/config/HibernateLoggerHints.java
+++ src/main/java/contratoscliente/graalvm/config/HibernateLoggerHints.java
@@ -5,7 +5,7 @@ import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.aot.hint.TypeReference;
 
-public class Hints implements RuntimeHintsRegistrar {
+public class HibernateLoggerHints implements RuntimeHintsRegistrar {
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
         hints.reflection().registerType(TypeReference.of(EntityManagerMessageLogger.class),
